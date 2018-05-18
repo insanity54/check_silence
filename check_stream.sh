6efef7380a34e4f8975480f7756e7c2a2658098f
Chis Grimmett <chris@grimtech.net>
2018-05-18 01:56:03 -0700

    add proper nagios plugin exit codes

--- check_stream.sh
+++ check_stream.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+# TITLE check_stream
+# DESCRIPTION Check an audio stream for silence
+# IMPORTANT URL must NOT end with .mp3.m3u
+
+
 failDepCheck() {
-        echo "CRITICAL - check_stream depends on ${1} which does not exist on this system!"
-        exit 5
+	echo "CRITICAL - check_stream depends on ${1} which does not exist on this system!"
+	exit 5
 }
 
 
@@ -17,23 +22,25 @@ command -v sed    >/dev/null 2>&1 || failDepCheck 'sed'
 
 if [ -z "$1" ]; then
     echo "Usage: ./check_stream.sh URL [aframes]"
-    exit 1
+    exit 7
 fi
 
 if [ -z "$2" ]; then aframes=500; else aframes=$2; fi
+
 
 # download a snippet of the livestream
 ffmpeg -loglevel quiet -y -i $1 -aframes $aframes /tmp/livesample.mp3
 
 # check to see if the snippet contains 3 seconds of silence
-check=$(ffmpeg -i /tmp/livesample.mp3 -af silencedetect=n=-25dB:d=3 -f null - 2>&1 | grep -Eo$
+check=$(ffmpeg -i /tmp/livesample.mp3 -af silencedetect=n=-25dB:d=3 -f null - 2>&1 | grep -Eo "silence_(start|end)" | tail -n1 | wc -l | sed 's/^ *//')
+
+# clear temp audio file
+test -f /tmp/livesample && rm /tmp/livesample.mp3
 
 if [[ $check -eq 1 ]]; then
     echo "CRITICAL - Substantial silence detected in stream"
+    exit 2
 else
     echo "OK - No substantial silence detected in stream"
+    exit 0
 fi
-
-
-test -f /tmp/livesample && rm /tmp/livesample.mp3
-exit 0
